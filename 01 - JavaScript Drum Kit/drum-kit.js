e947c5924aeb04c49b3019fb0bb6d38ab1d805c5
mpaternostro <marcelodpaternostro@gmail.com>
2019-11-17 19:46:36 -0300

    Ejercicio 1 completo

--- 01 - JavaScript Drum Kit/drum-kit.js
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -1,9 +1,18 @@
-const keys = document.querySelector('.keys');
-const audios = document.querySelectorAll('audio');
-keys.addEventListener("keydown", reproducirSonido());
-
-function reproducirSonido() {
-    const key = document.querySelector('div[data-key="68"');
-    console.log(key);
-
+function playSound(e) {
+    const keyCode = e.keyCode;
+    const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${keyCode}']`);
+    const keys = document.querySelectorAll('.key');
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 }
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+window.addEventListener("keydown", playSound);
